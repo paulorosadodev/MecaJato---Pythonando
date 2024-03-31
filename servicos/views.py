65e3fba8c949b3e0bd1a0e9a74f10bdb22c19c6b
Paulo Rosado <paulohenrique110803@gmail.com>
2024-03-30 22:03:58 -0300

    Aula 06

--- servicos/views.py
+++ servicos/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .forms import FormServico
+from django.http import HttpResponse
 # Create your views here.
 def novo_servico(request):
-    return render(request, 'novo_servico.html')
+    if request.method == 'GET':
+        form = FormServico()
+        ctx = {'form': form}
+        return render(request, 'novo_servico.html', ctx)
+    elif request.method == 'POST':
+        form = FormServico(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Salvo com sucesso')
+        else:
+            ctx = {'form': form}
+            return render(request, 'novo_servico.html', ctx)
+        
